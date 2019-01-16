533d97a87733df8d445c810082726b2be3b55921
LonglyWolf <814484626@qq.com>
2019-01-16 19:04:18 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yilu/ActivityStart.java
@@ -0,0 +1,44 @@
+package com.example.yilu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.yilu.NewUser.FourPictureToWelcome;
+
+public class ActivityStart extends AppCompatActivity {
+    Button start;
+    MianClickListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.start_activity);
+        iniInitialize();
+        iniClick();//绑定监听事件
+    }
+
+    public  void iniInitialize(){
+        listener = new MianClickListener();
+        start = (Button) findViewById(R.id.start);
+    }
+
+    public void iniClick(){
+        start.setOnClickListener(listener);
+    }
+
+    public class MianClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == start.getId()){
+                startActivity(new Intent(ActivityStart.this,FourPictureToWelcome.class));
+            }
+        }
+    }
+
+}
+
+
---
+++ app/src/main/java/com/example/yilu/MainActivities/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.yilu.MainActivities;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.example.yilu.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_fragment,fragment);
+        transaction.commit();
+    }
+}
